0ac1b55fd24b6601d2813304009c497d89f873a2
matsfunk <mathias.funk@gmail.com>
2016-01-23 22:38:01 +0100

    start of version control  - added an isConnected method to websockets api  - changed logging

---
+++ src/oocsi-web.js
@@ -0,0 +1,139 @@
+var OOCSI = (function() {
+
+	var wsUri = "ws://localhost:9000/";
+	var username;
+	var handlers = {};
+	var websocket;
+	var connected = false;
+	var logger = internalLog;
+
+	function init() {
+		logger("CONNECTING to "  + wsUri);
+		websocket = new WebSocket(wsUri);
+		websocket.onopen = function(evt) {
+			onOpen(evt)
+		};
+		websocket.onclose = function(evt) {
+			onClose(evt)
+		};
+		websocket.onmessage = function(evt) {
+			onMessage(evt)
+		};
+		websocket.onerror = function(evt) {
+			onError(evt)
+		};
+	}
+
+	function onOpen(evt) {
+		if(websocket.readyState == WebSocket.OPEN) {
+			submit(username);
+			connected = true;
+		}	 
+		logger("CONNECTED");
+	}
+
+	function onClose(evt) {
+		logger("DISCONNECTED");
+		connected = false;
+	}
+
+	function onMessage(evt) {
+		websocket.send(".");
+		if(evt.data != 'ping') {
+			try {
+				var e = JSON.parse(evt.data);
+				if(handlers[e.recipient] !== undefined) {
+					handlers[e.recipient](e);
+				} else {
+					logger('no handler for event: ' + evt.data);
+				}
+			} catch(e) {
+				logger('ERROR: parse exception for event data ' + evt.data);
+			}
+			logger('RESPONSE: ' + evt.data);
+		}
+	}
+
+	function onError(evt) {
+		logger('ERROR: ' + evt.data);
+	}
+
+	function waitForSocket(fn) {
+		if(!websocket || websocket.readyState == WebSocket.CONNECTING) {
+			setTimeout(function() { waitForSocket(fn) }, 200);
+		} else {
+			fn();
+		}
+	} 
+
+	function internalClose() {
+		websocket && websocket.close();
+	}
+
+	function submit(message) {
+		if(websocket && websocket.send(message)) {
+			logger("SENT: " + message);	
+		}
+	}
+
+	function internalLog(message) {
+		// do nothing by default
+	}
+
+	function internalSend(client, data) {
+		connected && submit('sendjson ' + client + ' '+ JSON.stringify(data));
+	} 
+
+	function internalSubscribe(channel, fn) {
+		if(connected) {
+			submit('subscribe ' + channel);
+			handlers[channel] = fn;
+		} 
+	} 
+
+	function internalUnsubscribe(channel) {
+		if(connected) {
+			submit('unsubscribe ' + channel);
+			handlers[channel] = function() {};
+		}
+	} 
+
+	return {
+		connect: function(server, clientName, fn) {
+			wsUri = server;
+			username = clientName && clientName.length > 0 ? clientName : "webclient_" + +(new Date());
+			handlers[clientName] = fn;
+			init();
+		},
+		send: function(recipient, data) {
+			waitForSocket(function() {
+				internalSend(recipient, data);
+			});
+		},
+		subscribe: function(channel, fn) {
+			waitForSocket(function() {
+				internalSubscribe(channel, fn);
+			});
+		},
+		unsubscribe: function(channel) {
+			waitForSocket(function() {
+				internalUnsubscribe(channel);
+			});
+		},
+		isConnected: function() {
+			return connected;
+		},
+		close: function() {
+			waitForSocket(function() {
+				internalClose();
+			});
+		},
+		handlers: function() {
+			return handlers;
+		},
+		logger: function(fn) {
+			logger = fn;
+		}
+	};
+
+})();
